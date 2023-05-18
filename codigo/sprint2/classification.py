226d722e0a81a8559e181ecc0759ec0145d141b9
unknown <lucas.ferreira057@bandtec.com.br>
2023-05-17 21:05:17 -0300

    criando funcao de modelo na classificao

--- codigo/sprint2/classification.py
+++ codigo/sprint2/classification.py
@@ -2,10 +2,53 @@ import json
 import os
 import glob
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+def plot(model, artist_list, X_test, y_test):
+    unique_artists, artist_counts = np.unique(artist_list, return_counts=True)
+
+    predicted_artists = model.predict(X_test)
+    unique_predicted_artists, predicted_artist_counts = np.unique(predicted_artists, return_counts=True)
+
+    total_count = sum(predicted_artist_counts)
+    percentages = [count / total_count * 100 for count in predicted_artist_counts]
+
+    plt.bar(unique_predicted_artists, percentages)
+    plt.title('Porcentagem dos Artistas Previstos (Decision Tree)')
+    plt.xlabel('Artista Previsto')
+    plt.ylabel('Porcentagem')
+    plt.xticks(rotation=90)
+    plt.show()
+
+    predicted_artists = model.predict(X_test)
+    unique_artists = np.unique(np.concatenate((y_test, predicted_artists), axis=0))
+    conf_matrix = confusion_matrix(y_test, predicted_artists)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    im = ax.imshow(conf_matrix, cmap='Blues')
+
+    ax.set_xticks(np.arange(len(unique_artists)))
+    ax.set_yticks(np.arange(len(unique_artists)))
+    ax.set_xticklabels(unique_artists, rotation=45, ha='right')
+    ax.set_yticklabels(unique_artists)
+
+    # Adicionar valores nas células da matriz
+    for i in range(len(unique_artists)):
+        for j in range(len(unique_artists)):
+            ax.text(j, i, conf_matrix[i, j], ha='center', va='center', color='white')
+
+    plt.title('Matriz de Confusão (Decision Tree)')
+    plt.colorbar(im, label='Contagem')
+    plt.xlabel('Artista Previsto')
+    plt.ylabel('Artista Real')
+
+    plt.tight_layout()
+    plt.show()
 
 def main():
     
@@ -52,14 +95,14 @@ def main():
 
         average_color_list[i] = np.array(average_color_list[i])
 
-    print("Starting model training...")
+    print("Starting model preparation...")
     X = np.column_stack((signature_list, average_color_list))
     y = np.array(artist_list)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=420)
-    print("Model trained successfully")
 
 
+    print("Started Model training")
     print("Training Decision Tree...")
     tree_model = DecisionTreeClassifier()
     tree_model.fit(X_train, y_train)
@@ -75,5 +118,7 @@ def main():
     forest_predictions = forest_model.predict(X_test)
     forest_accuracy = accuracy_score(y_test, forest_predictions)
     print("Accuracy (Random Forest):", forest_accuracy)
-
+    
+    plot(tree_model, artist_list, X_test, y_test)
+    plot(forest_model, artist_list, X_test, y_test)
 main()
