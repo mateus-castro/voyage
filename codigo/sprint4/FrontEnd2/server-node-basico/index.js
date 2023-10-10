d8aa111b013baaf934971ec758af0429f889d0b1
Gabriel Gameiro <gabriel.perez@bandtec.com.br>
2023-10-09 21:55:46 -0300

    Frontend se comunicando com o Backend

--- codigo/sprint4/FrontEnd2/server-node-basico/index.js
+++ codigo/sprint4/FrontEnd2/server-node-basico/index.js
@@ -2,14 +2,22 @@ const app = require('express')();
 const http = require('http');
 const swagger = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
+const bodyParser = require('body-parser');
+var fileupload = require("express-fileupload");
 
 // Iniciando server
 http.createServer(app).listen('80');
 console.log("Server iniciado na porta :80");
 console.log("Swagger na porta :80/docs");
 
+// Habilitando leitura do body
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(fileupload());
+
 // Subindo swagger gerado
 app.use('/docs', swagger.serve, swagger.setup(swaggerFile));
+
 
 // Todos arquivos de rotas devem ser declarados abaixo
 // Tambem devem ser declarados no swagger.js para documentacao
--- codigo/sprint4/FrontEnd2/server-node-basico/rotas/padrao.js
+++ codigo/sprint4/FrontEnd2/server-node-basico/rotas/padrao.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
     const path = require('path');
+    const request = require('request');
+    const serverConfig = require('../appSettings.json');
+    const fs = require("fs");
 
     // Formula padrao para endpoints
     app.get('/health-check', (req, res) => {
@@ -10,20 +13,69 @@ module.exports = (app) => {
         // #swagger.tags = ['Processamento de imagens']
         // #swagger.description = 'Endpoint para processamento da imagem no modelo da Voyager.'
         /*
-            #swagger.parameters['singleFile'] = {
+            #swagger.parameters['imagem'] = {
                 type: 'file',
                 required: 'true',
                 description: 'Imagem a ser processada.',
             } 
         */
+        const url = serverConfig.urlBackend + "/processar_img/";
+        console.log("URL Backend: " + url);
 
-        // Retorno que virá da API Python
-        const retorno = {
-            "origem": "Humano",
-            "aderencia": "80%",
-            "acuracia": "90%",
-        };
-        res.status(200).send(retorno);
+        console.log("Salvando imagem...");
+
+        // Apos isso, usar o fs pra ler de volta e jogar no corpo
+        req.files.imagem.mv('./rotas/arquivos/temp/images/upload.jpg', function (err) {
+            if (err) {
+                console.log("Erro ao salvar imagem!");
+                return res.status(500).send(err);
+            }
+            
+            console.log("Sucesso ao salvar imagem!");
+            console.log("Chamando Backend...");
+            request.post({
+                url,
+                formData: {
+                    "imagem": fs.createReadStream('./rotas/arquivos/temp/images/upload.jpg')
+                }
+            },
+                (err, responseBack, body) => {
+                    if (err) {
+                        console.log("Erro ao chamar Backend!");
+                    }
+
+                    if (responseBack.statusCode === 200) {
+                        console.log("Sucesso ao chamar Backend!");
+                    }
+
+                    res.status(responseBack.statusCode).send(body);
+                });
+        });
+    });
+
+    app.get('/dados-modelo', (req, res) => {
+        // #swagger.tags = ['Processamento de imagens']
+        // #swagger.description = 'Endpoint para verificar acuracidade atual do modelo usado pelo projeto.'
+        const url = serverConfig.urlBackend + "/main_model";
+        console.log("Chamando URL: " + url);
+        const options = {}
+
+        request.get(
+            url,
+            options,
+            (err, response, body) => {
+                if (err) {
+                    console.log("Erro!");
+                }
+
+                if (response.statusCode === 200) {
+                    console.log("Sucesso!");
+                }
+
+                console.log("Body:");
+                console.log(body);
+                res.status(response.statusCode).send(body);
+            });
     });
 
     app.get('/', (req, res) => {
